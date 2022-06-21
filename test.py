c29661c66db77d63d60f40963218b25007fbc632
teodiracho <106830050+teodiracho@users.noreply.github.com>
2022-06-22 03:11:56 +0800

    Update test.py
    update

--- test.py
+++ test.py
@@ -14,39 +14,3 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Display an image on Vector's face
-"""
-
-import os
-import sys
-import time
-
-try:
-    from PIL import Image
-except ImportError:
-    sys.exit("Cannot import from PIL: Do `pip3 install --user Pillow` to install")
-
-import anki_vector
-from anki_vector.util import degrees
-
-
-def main():
-    args = anki_vector.util.parse_command_args()
-
-    with anki_vector.Robot(args.serial) as robot:
-
-        ## SAY 'Happy Birthday'
-        print("Say 'Happy Birthday'...")
-        robot.behavior.say_text("happy birthday, tito christian")
-        
-        # If necessary, move Vector's Head and Lift to make it easy to see his face
-        # robot.behavior.set_head_angle(degrees(45.0))
-        # robot.behavior.set_lift_height(0.0)
-       
-        ## FIREWORKS
-        #animation = "anim_holiday_hny_fireworks_01"
-        #robot.anim.play_animation("anim_holiday_hny_fireworks_01")
-
-
-if __name__ == "__main__":
-    main()
